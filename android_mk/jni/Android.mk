95a20ced5694b8e36a77ba00bddf8095891ea85c
Glenn Kasten <gkasten@google.com>
2016-11-15 09:07:19 -0800

    Remove references to unsupported platform libraries
    libcutils and libutils are not available to an NDK build
    
    Test: re-build using ndk-build, there should be no undefined symbols
    Change-Id: I1186e3edc1ec09e3e795a42e597c2868449596dd

--- android_mk/jni/Android.mk
+++ android_mk/jni/Android.mk
@@ -6,6 +6,6 @@ LOCAL_C_INCLUDES := \
     ../source/
 LOCAL_SRC_FILES:= ../../apps/synthmark.cpp
 LOCAL_CFLAGS += -g -std=c++11 -Ofast
-LOCAL_SHARED_LIBRARIES := libcutils libutils
+#LOCAL_SHARED_LIBRARIES := libcutils libutils
 LOCAL_MODULE := synthmark
 include $(BUILD_EXECUTABLE)
