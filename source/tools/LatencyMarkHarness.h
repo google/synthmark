89c3d62b6601b8dd9d4134db5ee570836402cb5e
Phil Burk <philburk@google.com>
2020-01-29 10:38:09 -0800

    auto: print CDD summary
    Use language that matches the Q Android CDD.
    
    Fixes #91

--- source/tools/LatencyMarkHarness.h
+++ source/tools/LatencyMarkHarness.h
@@ -106,6 +106,7 @@ public:
 
     int32_t searchForLowestLatency(int32_t sampleRate, int32_t framesPerBurst, int32_t maxSeconds) {
         int32_t testCount = 1;
+        resetBinarySearch();
         int32_t bursts = getNextBurstsToTry(true); // assume zero latency glitches
 
         while (bursts > 0) {
